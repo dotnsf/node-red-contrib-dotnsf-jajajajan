34c8f40034de8032cab00c5fba441754fa8c3f95
K.Kimura <dotnsf@jp.ibm.com>
2017-12-28 23:31:22 +0000

    change node.send behavior

--- jajaja.js
+++ jajaja.js
@@ -1,3 +1,5 @@
+var j2r = require( './japanese2rhythm' );
+
 module.exports = function( RED ){
   function HashNode( config ){
     RED.nodes.createNode( this, config );
@@ -7,114 +9,47 @@ module.exports = function( RED ){
       var text = msg.payload;
 
       var newlines = [];
-      var rests = [ "、", "。", ",", "." ];
-      var seconds = [ 'ぁ', 'ゃ' ];
-      var arr = [ 'ず', 'た', 'だ', 'て', 'と', 'ど', 'ば', 'ぱ', 'ら' ];
-      var nn = [ 'ん' ];
       var lines = text.split( /[\n\r]/ );
       for( i = 0; i < lines.length; i ++ ){
         var line = lines[i];
-        line = kana2hira( line );
+        //line = kana2hira( line );
 
         if( line != "" ){
-          //. (1) 休符
-          for( j = 0; j < rests.length; j ++ ){
-            var tgt1 = rests[j];
-            while( line.indexOf( tgt1 ) > -1 ){
-              line = replaceOne( line, tgt1, 'R' );
-            }
-          }
+          var line2 = j2r.japanese2rhythm( line );
+          line2 = line2.substring( 0, line2.length - 1 );
 
-          for( j = 0; j < seconds.length; j ++ ){
-            var second = seconds[j];
-            while( line.indexOf( second ) > 0 ){
-              var idx = line.indexOf( second );
-              var first = line.substring( idx - 1, idx );
-    
-              if( arr.indexOf( first ) == -1 ){
-                arr.push( first );
-              }
-
-              //. (2) ４分
-              for( k = 0; k < nn.length; k ++ ){
-                var tgt2 = first + second + nn[k];
-                while( line.indexOf( tgt2 ) > -1 ){
-                  line = replaceOne( line, tgt2, '4' );
-                }
-              }
-
-              //. (3) ２分
-              for( k = 0; k < nn.length; k ++ ){
-                var tgt3 = first + second + 'ー' + nn[k];
-                while( line.indexOf( tgt3 ) > -1 ){
-                  line = replaceOne( line, tgt3, '2' );
-                }
-              }
-
-              //. (4) ８分
-              var tgt4 = first + second
-              while( line.indexOf( tgt4 ) > -1 ){
-                line = replaceOne( line, tgt4, '8' );
-              }
-            }
-          }
-
-          for( j = 0; j < arr.length; j ++ ){
-            var first = arr[j];
-            while( line.indexOf( first ) > -1 ){
-              //. (2) ４分
-              for( k = 0; k < nn.length; k ++ ){
-                var tgt2 = first + nn[k];
-                while( line.indexOf( tgt2 ) > -1 ){
-                  line = replaceOne( line, tgt2, '4' );
-                }
-              }
-
-              //. (3) ２分
-              for( k = 0; k < nn.length; k ++ ){
-                var tgt3 = first + 'ー' + nn[k];
-                while( line.indexOf( tgt3 ) > -1 ){
-                  line = replaceOne( line, tgt3, '2' );
-                }
-              }
-
-              //. (4) ８分
-              var tgt4 = first;
-              while( line.indexOf( tgt4 ) > -1 ){
-                line = replaceOne( line, tgt4, '8' );
-              }
-            }
-          }
-
-          newlines.push( hira2kana( line ) );
+          newlines.push( line2 );
         }
       }
 
-      var result = newlines.join( ' ' ); //. '8882 8882'
+      var result = newlines.join( ',' ).split( ',' ); //. 'じゃ:8,じゃ:8,じゃ:8,じゃーん:2,じゃ:8,じゃ:8,じゃ:8,じゃーん:2'
       sendCharAt( result, node, 0 );
     });
   }
   RED.nodes.registerType( "jajajajan", HashNode );
 
 
-  function sendCharAt( str, node, idx ){
-    if( idx >= str.length ){
+  function sendCharAt( arr, node, idx ){
+    if( idx >= arr.length ){
       return;
     }else{
-      var c = str.charAt( idx );
+      var s = arr[idx];
+      var tmp = s.split( ':' );
+      var c = tmp[0];
+      var l = tmp[1];
       var n = -1;
       try{
-        n = parseInt( c );
+        n = parseInt( l );
       }catch( e ){
       }
 
       if( n > 0 ){
         var sec = 8 / n;
         
-        node.send( { payload: c } );
-        setTimeout( sendCharAt, sec * 500, str, node, idx + 1 );
+        node.send( { payload: c, rhythm: n } );
+        setTimeout( sendCharAt, sec * 500, arr, node, idx + 1 );
       }else{
-        sendCharAt( str, node, idx + 1 );
+        sendCharAt( arr, node, idx + 1 );
       }
     }
   }
